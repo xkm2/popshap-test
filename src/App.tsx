4b10dc5657f5271bdc2c03c030724351c71fbf1e
Xavier Messado <xkm2@njit.edu>
2021-11-02 20:18:59 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.scss';
+
+function App() {
+  return (
+    <Router>
+
+    </Router>
+  );
+}
+
+export default App;
