d5ba28eb012abdb0c25964864141841fad772546
Xavier Messado <xkm2@njit.edu>
2021-11-09 18:12:45 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/Home';
 import StorePage from './pages/Store';
 import FormPage from './pages/Form';
